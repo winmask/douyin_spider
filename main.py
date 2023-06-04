cbff71842a53d60f3d3dc823389bc82779486e4a
raindrop <72308008+raindrop-hb@users.noreply.github.com>
2023-06-04 14:17:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ def main():
     print("github开源地址:https://github.com/raindrop-hb/douyin_spider\n使用请保留版权\n欢迎使用raindrop抖音爬虫_解析工具")
     ex = 1
     try:
-        aa = get("http://dy.hanbao16.top/remote.json",timeout=4)
+        aa = get("",timeout=4)
         aa=aa.json()
         if aa["version"]>1.02:
             print("检测到更新:\n更新版本号:{}\n更新内容:{}".format(aa["version"], aa["content"]))
